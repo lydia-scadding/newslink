032c746fd0a289ec0fdd0d312c73a645334ad600
Lydia Scadding <lydiascadding@gmail.com>
2021-06-10 23:12:44 +0100

    Seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+User.destroy_all
+
+puts "Creating users..."
+ruby = User.create!(username: "rubyfan", password: "password")
+tim = User.create!(username: "timcook", password: "password")
+js = User.create!(username: "jsFiend", password: "password")
+
+puts "Creating links..."
+ruby.links.create!(title: "Ruby docs", url: "https://ruby-doc.org", description: "All the Ruby methods")
+tim.links.create!(title: "Official Apple UK Site", url: "https://www.apple.com/uk/", description: "my personal website")
+js.links.create!(title: "Javascript blog", url: "https://davidwalsh.name/", description: "loads of cool stuff about JavaScript")
+
+puts "Created #{User.count} users and #{Link.count} links"
