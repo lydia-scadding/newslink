8e141e2a156da46b61a8c1676a0ee9936c399fd3
Lydia Scadding <lydiascadding@gmail.com>
2021-06-10 08:14:12 +0100

    Email column nullable

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  private
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
 end
