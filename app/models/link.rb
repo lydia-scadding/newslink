61f645c3c18a5377af7fa3783790785cd2a297f4
Lydia Scadding <lydiascadding@gmail.com>
2021-06-10 12:05:35 +0100

    Added validations with tests for Link

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,9 @@
 class Link < ApplicationRecord
   belongs_to :user
+
+  validates :title,
+            presence: true,
+            uniqueness: { case_sensitive: false }
+
+  validates :url, format: { with: %r{\Ahttps?://} }
 end
--- test/models/link_test.rb
+++ test/models/link_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class LinkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "has a title" do
+    assert_equal "link1", links(:one).title
+  end
+
+  test "invalid if title is blank" do
+    link = Link.new(title: "")
+    link.valid?
+    assert_not link.errors[:title].empty?
+  end
+
+  test "invalid if title is already in use (case insensitive)" do
+    link = Link.new(title: "LINK1")
+    link.valid?
+    assert_not link.errors[:title].empty?
+  end
+
+  test "invalid if url is not in the right format" do
+    url = "NOT A REAL URL"
+    link = Link.new(title: "some article", url: url)
+    link.valid?
+    assert_not link.errors[:url].empty?
+  end
 end
