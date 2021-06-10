405141f860fd9ef7f4c7105c74e6d7946aa6dabd
Lydia Scadding <lydiascadding@gmail.com>
2021-06-10 15:42:17 +0100

    Validations done for models

--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 2021_06_10_140451) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: ""
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -44,6 +43,7 @@ ActiveRecord::Schema.define(version: 2021_06_10_140451) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
+    t.string "email"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
