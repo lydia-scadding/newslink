393158ceb75f38c469aa99367670f739cb2afe3a
Lydia Scadding <lydiascadding@gmail.com>
2021-06-10 15:34:34 +0100

    Wrote tests and validations for value in Vote

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,8 @@
 class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :link
+
+  validates :value,
+            presence: true,
+            inclusion: { in: [-1, 1] }
 end
--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class VoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "has a value" do
+    assert_equal 1, votes(:upvote).value
+  end
+
+  test "invalid if value is blank" do
+    vote = Vote.new
+    vote.valid?
+    assert_not vote.errors[:value].empty?
+  end
+
+  test "valid with a value of 1 or -1" do
+    [1, -1].each do |val|
+      vote = Vote.new(value: val)
+      vote.valid?
+      assert_empty vote.errors[:value]
+    end
+  end
+
+  test "invalid with a value other than 1 or -1" do
+    vote = Vote.new(value: 3)
+    vote.valid?
+    assert_not vote.errors[:value].empty?
+  end
 end
