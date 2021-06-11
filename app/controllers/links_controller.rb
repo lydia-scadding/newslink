a4e9a3fd28bbaed98a7f326551eaf7a7884943bd
Lydia Scadding <lydiascadding@gmail.com>
2021-06-11 11:32:20 +0100

    Basic link information on index page

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,7 +1,9 @@
 class LinksController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
-  def index; end
+  def index
+    @links = Link.all
+  end
 
   def show; end
 
