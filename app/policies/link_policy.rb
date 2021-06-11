19d6ef74063c0d90f9dcbe111e846767188cabf7
Lydia Scadding <lydiascadding@gmail.com>
2021-06-11 14:42:14 +0100

    Added conditional to show view for if user is logged in

--- app/policies/link_policy.rb
+++ app/policies/link_policy.rb
@@ -14,10 +14,16 @@ class LinkPolicy < ApplicationPolicy
   end
 
   def update?
-    user == record.user
+    user_is_owner_or_admin?
   end
 
   def destroy?
-    user == record.user
+    user_is_owner_or_admin?
+  end
+
+  private
+
+  def user_is_owner_or_admin?
+    user == record.user || user.admin?
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_10_140451) do
+ActiveRecord::Schema.define(version: 2021_06_11_133142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2021_06_10_140451) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
     t.string "email"
+    t.boolean "admin", default: false
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
