8eeca8fdf329cb10679f30e04170f241ccbc5c5c
Lydia Scadding <lydiascadding@gmail.com>
2021-06-13 13:30:50 +0100

    Generated CommentsController and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'links#index'
 
-  resources :links, except: :index
+  resources :links, except: :index do
+    resources :comments, only: [:create, :edit, :update, :destroy]
+  end
+
+  resources :comments, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
