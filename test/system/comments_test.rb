3287869587d3710573e3698b41b3b85bd9c218bd
Lydia Scadding <lydiascadding@gmail.com>
2021-06-19 21:39:12 +0100

    Comments div is present even if empty

--- test/system/comments_test.rb
+++ test/system/comments_test.rb
@@ -7,36 +7,36 @@ class CommentsTest < ApplicationSystemTestCase
     @comment = comments(:comment1)
   end
 
-  # test "can create a comment" do
-  #   visit link_url(@link)
+  test "can create a comment" do
+    visit link_url(@link)
 
-  #   fill_in "Enter comment", with: "New Comment"
-  #   click_on "Create Comment"
+    fill_in "Enter comment", with: "New Comment"
+    click_on "Create Comment"
 
-  #   assert_text "New Comment"
-  # end
+    assert_text "New Comment"
+  end
 
-  # test "can update a comment" do
-  #   visit link_url(@comment.link)
+  test "can update a comment" do
+    visit link_url(@comment.link)
 
-  #   within("#comment-#{@comment.id}") do
-  #     click_on "Edit"
-  #   end
-  #   fill_in "Enter comment", with: "Updated text"
-  #   click_on "Update Comment"
+    within("#comment-#{@comment.id}") do
+      click_on "Edit"
+    end
+    fill_in "Enter comment", with: "Updated text"
+    click_on "Update Comment"
 
-  #   assert_text "Updated text"
-  # end
+    assert_text "Updated text"
+  end
 
-  # test "can delete a comment" do
-  #   visit link_url(@comment.link)
+  test "can delete a comment" do
+    visit link_url(@comment.link)
 
-  #   accept_confirm do
-  #     within("#comment-#{@comment.id}") { click_on "Delete" }
-  #   end
+    accept_confirm do
+      within("#comment-#{@comment.id}") { click_on "Delete" }
+    end
 
-  #   assert_current_path(link_path(@comment.link))
-  # end
+    assert_current_path(link_path(@comment.link))
+  end
 
   test "all comments on a link are displayed on show page" do
     visit link_url(@link)
