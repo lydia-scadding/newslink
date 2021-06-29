767464595b3e60fb91fb70df0fc3f884df361cff
Lydia Scadding <lydiascadding@gmail.com>
2021-06-29 22:20:52 +0100

    Put parent link on comments on comments page

---
+++ app/helpers/comments_helper.rb
@@ -0,0 +1,9 @@
+module CommentsHelper
+  def parent_link(comment)
+    if params[:controller] == "comments"
+      content_tag :span, class: "parent-link" do
+        " | on: #{link_to raw(comment.link.title), link_path(comment.link)}".html_safe
+      end
+    end
+  end
+end
