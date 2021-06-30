c55d4bffb381145c2084c2f7311bd10ed39d64d0
Lydia Scadding <lydiascadding@gmail.com>
2021-06-30 11:48:42 +0100

    Parent links only display on comments index page

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,6 +1,6 @@
 module CommentsHelper
   def parent_link(comment)
-    if params[:controller] == "comments"
+    if params[:controller] == "comments" && params[:action] == "index"
       content_tag :span, class: "parent-link" do
         " | on: #{link_to raw(comment.link.title), link_path(comment.link)}".html_safe
       end
