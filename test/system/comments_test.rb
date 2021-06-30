08df47d8228bb433ea3a07d1143667a283b59c3c
Lydia Scadding <lydiascadding@gmail.com>
2021-06-30 14:49:28 +0100

    Uncomment comment test

--- test/system/comments_test.rb
+++ test/system/comments_test.rb
@@ -7,57 +7,57 @@ class CommentsTest < ApplicationSystemTestCase
     @comment = comments(:comment1)
   end
 
-  # test "can create a comment" do
-  #   visit link_url(@link)
+  test "can create a comment" do
+    visit link_url(@link)
 
-  #   fill_in "Enter comment", with: "New Comment"
-  #   click_on "Create Comment"
+    fill_in "Enter comment", with: "New Comment"
+    click_on "Create Comment"
 
-  #   assert_text "New Comment"
-  # end
+    assert_text "New Comment"
+  end
 
-  # test "can update a comment" do
-  #   visit link_url(@comment.link)
+  test "can update a comment" do
+    visit link_url(@comment.link)
 
-  #   within("#comment-#{@comment.id}") do
-  #     click_on "edit"
-  #   end
-  #   fill_in "Enter comment", with: "Updated text"
-  #   click_on "Update Comment"
+    within("#comment-#{@comment.id}") do
+      click_on "edit"
+    end
+    fill_in "Enter comment", with: "Updated text"
+    click_on "Update Comment"
 
-  #   assert_text "Updated text"
-  # end
+    assert_text "Updated text"
+  end
 
-  # test "can delete a comment" do
-  #   visit link_url(@comment.link)
+  test "can delete a comment" do
+    visit link_url(@comment.link)
 
-  #   accept_confirm do
-  #     within("#comment-#{@comment.id}") { click_on "delete" }
-  #   end
+    accept_confirm do
+      within("#comment-#{@comment.id}") { click_on "delete" }
+    end
 
-  #   assert_current_path(link_path(@comment.link))
-  # end
+    assert_current_path(link_path(@comment.link))
+  end
 
-  # test "can view all comments" do
-  #   visit root_url
+  test "can view all comments" do
+    visit root_url
 
-  #   within(".navbar") { click_on "comments" }
-  #   assert_selector ".comment-card", count: Comment.all.size
-  # end
+    within(".navbar") { click_on "comments" }
+    assert_selector ".comment-card", count: Comment.all.size
+  end
 
-  # test "all comments on a link are displayed on show page" do
-  #   visit link_url(@link)
+  test "all comments on a link are displayed on show page" do
+    visit link_url(@link)
 
-  #   assert_selector ".comment-card", count: @link.comments.size
-  # end
+    assert_selector ".comment-card", count: @link.comments.size
+  end
 
-  # test "comments form rerenders with errors" do
-  #   visit link_url(@link)
+  test "comments form rerenders with errors" do
+    visit link_url(@link)
 
-  #   click_on "Create Comment"
-  #   assert_selector ".is-invalid"
-  #   assert_selector ".invalid-feedback"
-  # end
+    click_on "Create Comment"
+    assert_selector ".is-invalid"
+    assert_selector ".invalid-feedback"
+  end
 
   test "redirected if tries to create a comment when not logged in" do
     sign_out :user
